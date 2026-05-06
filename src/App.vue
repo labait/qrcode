4cbe6ae35348d1c912b34fb886bb2154b063931f
Antonio Molinari <antoniomolinari@me.com>
2026-05-06 12:05:28 +0200

    Add Tailwind CSS integration and update components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <main class="mx-auto max-w-2xl flex flex-col items-center justify-center">
+    <h1 class="text-2xl font-bold mb-4">Hello World</h1>
+    <Test />
+  </main>
 </template>
